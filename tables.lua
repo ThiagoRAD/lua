3f08a73d1449fd3d69b42f5c1c1791ad3c19f1d8
ThiagoRAD <thauroo@gmail.com>
2022-12-20 13:48:39 -0300

    tables

---
+++ tables.lua
@@ -0,0 +1,7 @@
+characters = {
+  heroes = {"link", "zelda", "mario"},
+  villains = {"ganondorf", "bowser", "dracula"}
+}
+
+print("Second hero: " .. characters.heroes[2])  -- Accessing the second hero
+print("Third villain: " .. characters.villains[3])  -- Accessing the third villain
