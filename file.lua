d7a4ee165d6e01c1de68870de904cb0d7784bf8b
ThiagoRAD <thauroo@gmail.com>
2024-02-24 19:13:49 -0300

    lua notes

--- file.lua
+++ file.lua
@@ -1,1 +1,68 @@
-print("Welcome hero!")
+multiline_string = [[
+This is a multiline
+string in Lua.
+It can span multiple lines.
+]]
+
+
+
+some_text = "Hello, Lua!"
+some_text = "Hello... Lua!"
+print(some_text)
+
+
+-- kills = 20
+-- assists = 15
+-- deaths = 5
+
+-- kda_ratio = (kills + assists) / deaths
+-- print("Your KDA ratio is: ", kda_ratio)
+
+
+
+
+-- MAX_LIVES = 3
+-- name = "Link"
+-- character_color = "Green"
+-- age = 17
+-- has_sword = true
+-- speed = 2
+-- companion_name = "Shield"
+
+-- print(MAX_LIVES)
+-- print(name)
+-- print(character_color)
+-- print(age)
+-- print(has_sword)
+-- print(speed)
+-- print(companion_name)
+
+
+-- type(nil)         -- nil, the absence of a value
+-- type(true)        -- boolean
+-- type(10.4 * 12)   -- number
+-- type("welcome")   -- string
+-- type(print)       -- function
+-- type({})          -- table
+-- type(io.stdin)    -- userdata
+
+-- potion = "blueberry cream" 
+
+-- type(potion)         --> string
+-- print(potion)        --> Output: blueberry cream
+
+-- potion_weight = 56
+
+-- type(grams)          --> number
+-- print(grams)         --> Output: 56
+
+-- is_deadly = false
+
+-- type(is_deadly)      --> boolean
+-- print(is_deadly)     --> Output: false
+
+-- -- we can also initialize and declare multiple variables at a time
+-- questions, answers = 10, 10
+
+-- print(questions) 
+-- print(answers)
