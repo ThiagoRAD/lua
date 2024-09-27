fa4f3c611f4e55122a1bf6370beb8bc6c2a3deb9
ThiagoRAD <thauroo@gmail.com>
2024-09-27 13:34:08 -0300

    if

---
+++ if.lua
@@ -0,0 +1,12 @@
+-- Change GAME_LANG below to test! 
+
+GAME_LANG = 2
+
+-- Write code below 💖
+if GAME_LANG == 1 then
+  print("Welcome to your world, Hero")
+elseif GAME_LANG == 2 then
+  print("Bienvenido a tu mundo, Héroe.")
+else
+  print("Bem-vindo ao seu mundo, Herói.")
+end
